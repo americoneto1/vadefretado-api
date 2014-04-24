2346d286aaab386e8026cbedd8afbe7603c0cb37
americoneto1 <americoneto1@gmail.com>
2014-04-23 22:28:28 -0300

    Versao inicial da API

---
+++ server.js
@@ -0,0 +1,20 @@
+
+var restify = require('restify');
+var app = restify.createServer();
+var port = process.env.PORT || 8888;
+
+app.use(restify.queryParser());
+app.use(restify.jsonp());
+
+app.get("/api/associacoes/:id/linhas", function(req, res) {
+	//res.charSet('utf-8');
+	console.log('Request /api/associacoes/', req.params.id, '/linhas');
+	var API = require('./lib/FretadoApp.API.Associacoes');
+	API.getLinhas(req.params.id, function(associacao) {
+		res.send(associacao);
+	});
+});
+
+app.listen(port, function() { 
+	console.log('API running on', port, 'port'); 
+});
